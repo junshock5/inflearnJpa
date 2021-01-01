81fe945ae8ccb80902bc7aca4262911469d1c241
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-01 20:07:00 +0900

    create project jpa test

---
+++ src/main/java/com/junshock/jpatest/JpatestApplication.java
@@ -0,0 +1,13 @@
+package com.junshock.jpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatestApplication.class, args);
+	}
+
+}
