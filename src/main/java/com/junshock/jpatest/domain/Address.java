4bb5c626fdbacb2eaecb8d60b9be9120490d6111
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-02 19:27:10 +0900

    - jpa format_sql: true 설정되게 변경 - Address class 생성자 클래스 추가

--- src/main/java/com/junshock/jpatest/domain/Address.java
+++ src/main/java/com/junshock/jpatest/domain/Address.java
@@ -11,4 +11,15 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // JPA 스펙상 엔티티나 임데디드타입은 자바 기본생성자를 public or protected로 설정해야한다.
+    // 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용하게 지원해야 한다.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
