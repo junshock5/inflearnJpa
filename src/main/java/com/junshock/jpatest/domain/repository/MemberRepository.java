05d0bdd494440be1ee87f69da7bf06f6fff90012
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-04 02:19:32 +0900

    #10 주문, 상품, 멤버 비즈니스 로직 개발 - 회원 리포지토리 개발 ( 저장, 조회 ) - package 디렉토리 변경 - 잔디.bat 파일 추가 - readme.md 파일 추가

---
+++ src/main/java/com/junshock/jpatest/domain/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package com.junshock.jpatest.domain.repository;
+
+import com.junshock.jpatest.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // Spring이 EntityManager를 만들어서 주입.
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class) // JSQL 테이블이 아닌 엔터티 객체를 대상으로 질의
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
