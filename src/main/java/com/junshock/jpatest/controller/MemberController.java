17a2c4656909676d679caec99739e33db736fffa
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-13 17:57:19 +0900

    #14 홈 화면과 레이아웃 - h2 local 설정 변경 - createMemberForm.html view thymeleaf 추가 - MemberForm db valid과 view단 dto성 데이터와 분리를 위해 추가 - MemberController 에서 Get members Post members 메서드 추가

---
+++ src/main/java/com/junshock/jpatest/controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.junshock.jpatest.controller;
+
+import com.junshock.jpatest.domain.Address;
+import com.junshock.jpatest.domain.Member;
+import com.junshock.jpatest.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        // thymeleaf spring integration 되어 있어서 BindingResult가 있으면 에러 발생시 여기에 담고 코드 실행한다.
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
