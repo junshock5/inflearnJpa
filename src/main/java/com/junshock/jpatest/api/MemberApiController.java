1e2e93b80167e5d8b4808fffdc5a712f6173b384
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-19 16:13:44 +0900

    #18 회원 등록 수정 조회 API 개발 - 회원 수정 메서드 추가(영속성 컨텍스트 변경감지) - 쿼리 & 커멘드성 분리 하여 유지보수성을 높임

--- src/main/java/com/junshock/jpatest/api/MemberApiController.java
+++ src/main/java/com/junshock/jpatest/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.junshock.jpatest.api;
 
 import com.junshock.jpatest.domain.Member;
 import com.junshock.jpatest.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -20,6 +19,7 @@ public class MemberApiController {
     /**
      * 멤버 등록 API
      * 외부 API 스팩 변경시 모델 설계명 자체가 바뀌어 유연 하지 못한 구조
+     *
      * @param member
      * @return
      */
@@ -32,6 +32,7 @@ public class MemberApiController {
     /**
      * 멤버 등록 API
      * DTO 파라미터 로 한번 치환후 사용하기 떄문에 외부 변동에 유연한 구조
+     *
      * @param request
      * @return
      */
@@ -42,6 +43,34 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 멤버 수정 API
+     *
+     * @param id
+     * @param request
+     * @return
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
