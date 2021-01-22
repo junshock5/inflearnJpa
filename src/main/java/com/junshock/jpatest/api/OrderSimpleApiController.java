883d5d269bdaa184ce4cf048ac391929906d7480
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-01-22 14:57:23 +0900

    #20 조회 API 성능 최적화 - Lazy 로딩으로 인한 db n+1 쿼리 문제 (N + 1: order->member , order->delivery) 언급 - SimpleOrderDto를 사용해서 확장성 고려 (Entity를 직접사용하면 업데이트시 클라이언트 측 spec이 전부 바뀜) - join fetch JPQL을 사용하여 n+1 쿼리문제를 객체 그래프 join을 탐색하여 가져온 한방 쿼리 사용하여 성능문제 해결

--- src/main/java/com/junshock/jpatest/api/OrderSimpleApiController.java
+++ src/main/java/com/junshock/jpatest/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package com.junshock.jpatest.api;
 
+import com.junshock.jpatest.domain.dto.Address;
+import com.junshock.jpatest.domain.dto.OrderStatus;
 import com.junshock.jpatest.domain.order.Order;
 import com.junshock.jpatest.repository.OrderRepository;
 import com.junshock.jpatest.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -29,11 +34,56 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
-        for(Order order: all){
+        for (Order order : all) {
             order.getMember().getName(); //Lazy 강제 초기화
             order.getDelivery().getAddress(); //Lazy 강제 초기화
         }
 
         return all;
+    }
+
+    // Lazy 로딩으로 인한 db n+1 쿼리 문제 코드
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1: order->member , order->delivery 회원 n개를 가져 올때 배송 N번이 추가 쿼리가 발생한다.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    // Lazy 로딩으로 인한 db n+1 쿼리 문제 해결 코드 (fetch join 객체 그래프 사용)
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        //fetch 를 LAZY로 변경 후 fetch join을 이용해서 해결하자.
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
+
     }
 }
