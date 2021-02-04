c40971ea1a4c6624f37dfd7b09935807406da2ce
junshock5 <61732452+junshock5@users.noreply.github.com>
2021-02-04 20:18:16 +0900

    #24 OSIV와 성능 최적화 - open-in-view 설정에 따라 jpa가 영속선 컨텍스트 라이프 사이클을 mvc단 까지 관리하냐, 트랜잭션(Service,Repository) 까지만 관리하냐 를 설정할 수 있다. - 고객서비스의 실시간 APIS느 OSIV를 끈다. ADMIN처럼 커넥션 많이 사용하지 않는곳에서는 OSIV를 켠다.

---
+++ src/main/java/com/junshock/jpatest/service/query/OrderQueryService.java
@@ -0,0 +1,28 @@
+package com.junshock.jpatest.service.query;
+
+import com.junshock.jpatest.domain.order.Order;
+import com.junshock.jpatest.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+}
